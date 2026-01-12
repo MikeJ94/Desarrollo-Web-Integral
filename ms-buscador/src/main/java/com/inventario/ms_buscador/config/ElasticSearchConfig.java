86d8953c9995b722a7ec4786cdfd59d016e4ac32
MikeJ94 <mikerodruzgmail.com>
2026-01-12 00:05:42 -0600

    fix: compatibilidad con Bonsai Elasticsearch inyectando cabeceras

---
+++ ms-buscador/src/main/java/com/inventario/ms_buscador/config/ElasticSearchConfig.java
@@ -0,0 +1,42 @@
+package com.inventario.ms_buscador.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+// IMPORT CLAVE: Usamos el HttpHeaders específico de Spring Data Elasticsearch
+import org.springframework.data.elasticsearch.support.HttpHeaders; 
+import org.springframework.lang.NonNull;
+
+import java.util.function.Supplier;
+
+@Configuration
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Override
+    @NonNull
+    public ClientConfiguration clientConfiguration() {
+        // Limpiamos la URL (host:puerto)
+        String cleanUri = uris.replace("https://", "").replace("http://", "");
+
+        // Usamos la clase HttpHeaders que el driver espera
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Elastic-Product", "Elasticsearch");
+
+        return ClientConfiguration.builder()
+                .connectedTo(cleanUri)
+                .usingSsl()
+                .withBasicAuth(username, password)
+                .withHeaders(() -> headers)
+                .build();
+    }
+}
