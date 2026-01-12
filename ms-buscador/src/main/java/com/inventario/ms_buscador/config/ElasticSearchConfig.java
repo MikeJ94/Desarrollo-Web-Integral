7c8db83c81629e038290a814470fde521d1b2602
MikeJ94 <mikerodruzgmail.com>
2026-01-12 03:07:46 -0600

    fix: compatibilidad con Bonsai Elasticsearch inyectando cabeceras 16

--- ms-buscador/src/main/java/com/inventario/ms_buscador/config/ElasticSearchConfig.java
+++ ms-buscador/src/main/java/com/inventario/ms_buscador/config/ElasticSearchConfig.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.elasticsearch.support.HttpHeaders;
 import org.springframework.lang.NonNull;
 
 import java.time.Duration;
 
 @Configuration
+// Esta línea es VITAL para que no crashee buscando el ProductRepository
+@EnableElasticsearchRepositories(basePackages = "com.inventario.ms_buscador.repository")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     @Value("${spring.elasticsearch.uris}")
@@ -24,29 +27,21 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Override
     @NonNull
     public ClientConfiguration clientConfiguration() {
-        // 1. Limpieza profunda de la URI para obtener solo el HOST
-        // Bonsai devuelve a veces la URL con el puerto incluido o con / al final
-        String targetHost = uris.replace("https://", "")
-                               .replace("http://", "")
-                               .split("/")[0]; // Nos quedamos solo con la parte del dominio:puerto
+        String targetHost = uris.replace("https://", "").replace("http://", "").split("/")[0];
         
-        // 2. Forzar puerto 443 si es HTTPS y no hay puerto explícito
         if (!targetHost.contains(":") && uris.contains("https")) {
             targetHost += ":443";
         }
 
-        System.out.println("DEBUG: Conectando a Bonsai en host: " + targetHost);
-
-        // 3. Configuración de Headers de compatibilidad
-        // Esto le dice a Bonsai (ES 7.x) que ignore los comandos de ES 8.x
         HttpHeaders headers = new HttpHeaders();
+        // Forzamos compatibilidad para evitar el error 400 en Bonsai
         headers.add("Accept", "application/vnd.elasticsearch+json;compatible-with=7");
         headers.add("Content-Type", "application/vnd.elasticsearch+json;compatible-with=7");
         headers.add("X-Elastic-Product", "Elasticsearch");
 
         return ClientConfiguration.builder()
                 .connectedTo(targetHost)
-                .usingSsl() // Bonsai requiere SSL
+                .usingSsl()
                 .withBasicAuth(username, password)
                 .withHeaders(() -> headers)
                 .withConnectTimeout(Duration.ofSeconds(10))
