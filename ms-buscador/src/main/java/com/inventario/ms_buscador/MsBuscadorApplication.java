27d01a21d97b6e220cda9c8089fb57fbf3cdc75a
MikeJ94 <mikerodruzgmail.com>
2026-01-12 00:49:34 -0600

    fix: compatibilidad con Bonsai Elasticsearch inyectando cabeceras 6

--- ms-buscador/src/main/java/com/inventario/ms_buscador/MsBuscadorApplication.java
+++ ms-buscador/src/main/java/com/inventario/ms_buscador/MsBuscadorApplication.java
@@ -2,10 +2,15 @@ package com.inventario.ms_buscador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    ElasticsearchClientAutoConfiguration.class, 
+    ElasticsearchDataAutoConfiguration.class
+})
 
 @EnableDiscoveryClient // Habilita el registro en Eureka Serv
 //@EnableElasticsearchRepositories(basePackages = "com.inventario.ms_buscador.repository")
